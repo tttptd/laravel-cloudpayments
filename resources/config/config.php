db4a3aab91bb9e527cabf639047ad6afdc4cb18c
Yu Shi <mynsa04@ya.ru>
2017-08-31 20:29:54 +0300

    ++ config defaults

--- resources/config/config.php
+++ resources/config/config.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'publicId' => env('CLOUDPAYMENTS_PUBLIC_ID'),
-    'apiPassword' => env('CLOUDPAYMENTS_API_PASSWORD'),
+    'publicId' => env('CLOUDPAYMENTS_PUBLIC_ID', 'pk_000'),
+    'apiPassword' => env('CLOUDPAYMENTS_API_PASSWORD', '123'),
 
     // 1 — Упрощенная система налогообложения (Доход)
-    'taxationSystem' => env('CLOUDPAYMENTS_TAXATION_SYSTEM'),
+    'taxationSystem' => env('CLOUDPAYMENTS_TAXATION_SYSTEM', null),
 
 ];
